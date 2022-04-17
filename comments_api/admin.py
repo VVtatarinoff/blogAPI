7e647726ef8336d79f14be396f72ba4765faa63d
vladimir <vvtatarinoff@gmail.com>
2022-04-17 17:13:56 +0300

    admin panel

--- comments_api/admin.py
+++ comments_api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from comments_api.models import Article, Comments
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'body')
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'text', 'parent', 'article')
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comments, CommentsAdmin)
