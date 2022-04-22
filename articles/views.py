3bfbd27161a9a06999e212d4a49cace041683ccf
vladimir <vvtatarinoff@gmail.com>
2022-04-22 13:16:05 +0300

    update create article view

--- articles/views.py
+++ articles/views.py
@@ -1,23 +1,10 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
+
+from articles.models import Article
 from articles.serializers import ArticleCreateSerializer
 
 
-# Create your views here.
-
-class ArticleCreateView(APIView):
+class ArticleCreateView(CreateAPIView):
     """Добавление статьи"""
-
-    def post(self, request):
-        article = ArticleCreateSerializer(data=request.data)
-        if article.is_valid():
-            try:
-                article.save()
-            except Exception:
-                return Response({"error": {"code": 1,
-                                           "msg": "ошибка записи в БД"}},
-                                status=400)
-            return Response(status=201)
-        return Response({"error": {"code": 2,
-                                   "msg": article.errors['title']}},
-                        status=400)
+    serializer_class = ArticleCreateSerializer
+    queryset = Article.objects.all()
